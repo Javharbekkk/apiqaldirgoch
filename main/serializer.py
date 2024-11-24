d339b4c514c77ee5ac971ff2d3473d83bec064ca
Javharbekkk <javharbekabduvoitov@gmail.com>
2024-11-24 11:16:53 +0500

    Update serializer.py

--- main/serializer.py
+++ main/serializer.py
@@ -1,43 +1,51 @@
 from rest_framework import serializers
-
 from .models import *
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Register
         fields = '__all__'
 
+
 class ImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = '__all__'
+
 
 class TextSerializer(serializers.ModelSerializer):
     class Meta:
         model = TextHome
         fields = '__all__'
 
+
 class IMSJSerializer(serializers.ModelSerializer):
     class Meta:
         model = IMSJ
         fields = '__all__'
+
 
 class TextFillSerializer(serializers.ModelSerializer):
     class Meta:
         model = TextFill
         fields = '__all__'
 
+
 class IntelektVariantSerializer(serializers.ModelSerializer):
     class Meta:
         model = IntelektVariant
-        fields = ('id', 'matn', 'is_correct')  # is_correct maydoni qo'shildi
+        fields = ('id', 'matn', 'is_correct')  # Variant maydonlari
+
 
 class IntelektSerializer(serializers.ModelSerializer):
-    variantlar = IntelektVariantSerializer(many=True)
+    variantlar = IntelektVariantSerializer(many=True, source='intelektvariant_set')
+    # source: modeldagi bog'liqlikni ko'rsatadi
 
     class Meta:
         model = Intelekt
         fields = ('id', 'savol_matni', 'variantlar')
+
 
 class MaxsusVariantSerializer(serializers.ModelSerializer):
     class Meta:
@@ -46,12 +54,9 @@ class MaxsusVariantSerializer(serializers.ModelSerializer):
 
 
 class MaxsusSerializer(serializers.ModelSerializer):
-    variantlar = MaxsusVariantSerializer(many=True)
+    variantlar = MaxsusVariantSerializer(many=True, source='maxsusvariant_set')
+    # source: modeldagi bog'liqlikni ko'rsatadi
 
     class Meta:
         model = Maxsus
         fields = ('id', 'savol_matni', 'variantlar')
-
-
-
-
